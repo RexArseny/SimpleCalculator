80d9862f475ddc40817a44af3e0201a19a6da644
Арсений Каплин <kaplin_arseny@mail.ru>
2021-08-16 19:24:43 +0300

    feat: control of incorrect characters

--- SimpleCalculator/MainWindow.xaml.cs
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -12,89 +12,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace SimpleCalculator
 {
     public partial class MainWindow : Window
     {
-        private int[] a = new int[10];
-        private char[] b = new char[10];
-        private int c = 0;
+        private double result;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Digit_Input(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (sender is Button button)
             {
                 MainText.Text += button.Content;
-                a[c] *= 10;
-                a[c] += Convert.ToInt32(button.Content);
             }
-        }
-
-        private void Button_Click_plus(object sender, RoutedEventArgs e)
-        {
-            MainText.Text += "+";
-            b[c] = '+';
-            c++;
-        }
-
-        private void Button_Click_minus(object sender, RoutedEventArgs e)
-        {
-            MainText.Text += "-";
-            b[c] = '-';
-            c++;
-        }
-
-        private void Button_Click_multiply(object sender, RoutedEventArgs e)
-        {
-            MainText.Text += "*";
-            b[c] = '*';
-            c++;
-        }
-
-        private void Button_Click_divide(object sender, RoutedEventArgs e)
-        {
-            MainText.Text += "/";
-            b[c] = '/';
-            c++;
         }
 
         private void Button_Click_calculate(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < c; ++i)
+            bool error = false;
+            foreach(char character in MainText.Text)
             {
-                switch (b[i])
-                {
-                    case '+':
-                        a[i + 1] = a[i] + a[i + 1];
-                        break;
-                    case '-':
-                        a[i + 1] = a[i] - a[i + 1];
-                        break;
-                    case '*':
-                        a[i + 1] = a[i] * a[i + 1];
-                        break;
-                    case '/':
-                        a[i + 1] = a[i] / a[i + 1];
-                        break;
-                }
+                if (Char.IsDigit(character) || character == '+' || character == '-' || character == '*' || character == '/') continue;
+                else error = true;
             }
-            Answer.Text = Convert.ToString(a[c]);
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void Answer_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
+            if (error) Answer.Text = "Недопустимый символ";
+            else
+            {
+                result = Convert.ToDouble(new DataTable().Compute(MainText.Text, ""));
+                Answer.Text = Convert.ToString(result);
+            }
         }
     }
 }
